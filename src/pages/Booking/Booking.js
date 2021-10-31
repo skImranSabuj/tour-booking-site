11df6a713d57e28d2b95bb357a28a3980cf4ec3a
Sk Imran Sabuj <sabujx9@gmail.com>
2021-10-31 07:38:00 +0600

    booking  updates

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 import useCart from '../../hooks/usecart';
 import usePlaces from '../../hooks/usePlaces';
 import { addToDb } from '../../localStorage/tempDb';
 
 const Booking = () => {
+    const { user } = useAuth();
     const { register, handleSubmit, reset } = useForm();
     const [cart, setCart] = useCart();
     const [place, setPlace] = useState({});
@@ -20,15 +22,31 @@ const Booking = () => {
     const { place_name, key, description, days, about, thumb, country, rating, cost } = place;
 
     const onSubmit = data => {
-        // console.log(data);
-        handleAddToCart();
-        axios.post('http://localhost:5000/places', data)
-            .then(res => {
-                if (res.data.insertedId) {
-                    alert('added successfully');
-                    reset();
-                }
-            })
+        console.log(data, place);
+        const exists = cart.find(pd => pd._id === place._id);
+        let newCart = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd._id !== place._id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, place];
+            alert('Added to cart, had before');
+        }
+        else {
+            place.quantity = 1;
+            newCart = [...cart, place];
+            alert('Added to cart, new place');
+        }
+        setCart(newCart);
+        console.log(cart);
+        // save to local storage (for now)
+        addToDb(place._id);
+        // axios.post('http://localhost:5000/places', data)
+        //     .then(res => {
+        //         if (res.data.insertedId) {
+        //             alert('added successfully');
+        //             reset();
+        //         }
+        //     })
     }
     const handleAddToCart = () => {
         const exists = cart.find(pd => pd._id === place._id);
@@ -53,14 +71,10 @@ const Booking = () => {
         <div className="add-service">
             < h2 className="text-center"> Book and confirm your Trip</h2 >
             <form onSubmit={handleSubmit(onSubmit)}>
-                <label>Place Name:</label>
-                <input {...register("place_name")} defaultValue={country} />
+                <label>Place Name:{place_name}</label>
+                <input {...register("name")} defaultValue={user.email ? user.displayName : 'Your Name'} />
                 <input type="number" {...register("phone")} defaultValue={880} />
-                <input {...register("email", { required: true })} placeholder="Your Email" />
-
-                <input type="number" {...register("cost")} placeholder="Estimated Cost" />
-                <input type="number" {...register("days")} placeholder="how Long (Days)" />
-                <input type="number" {...register("rating")} placeholder="Rate the place out of 5" />
+                <input {...register("email", { required: true })} defaultValue={user.email ? user.email : 'Your Email'} />
 
                 <input type="submit" />
             </form>
